a6a4098ea811115dc42d3b59fa662ce6a0ad9ec3
FrMnJ <89046461+FrMnJ@users.noreply.github.com>
2025-05-20 18:39:18 -0600

    Azure config (#16)
    * fix: change service type from LoadBalancer to ClusterIP for internal access across multiple services
    
    * fix: update WebSocket URL to remove port for connection
    
    * feat: add Gateway and VirtualService configurations for taptalk.net
    
    * fix: update WebSocket and configuration references from taptalk.net to taptalk.tech
    
    * fix: update WebSocket and configuration references to taptalk.udgpath24.tech

--- tap-talk-front/src/pages/ChatPage.jsx
+++ tap-talk-front/src/pages/ChatPage.jsx
@@ -67,7 +67,7 @@ export default function ChatPage() {
   const connectToServer = () => {
     setConnecting(true);
     setMessages([]);
-    const ws = new WebSocket(`ws://taptalk.net:8765/ws/${userId.current}`);
+    const ws = new WebSocket(`ws://taptalk.udgpath24.tech/ws/${userId.current}`);
     setWebsocket(ws);
   };
 
